c16116bdde7eab0dee1b8eb58746cc5a0256db2b
Aldo Pagani <aldojpagani@gmail.com>
2025-05-22 19:14:23 -0600

    Displayed the tweets on the homepage, added some basic styling as well

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/models/tweet.dart';
 import 'package:twitter_clone/pages/create.dart';
 import 'package:twitter_clone/pages/settings.dart';
+import 'package:twitter_clone/providers/tweet_provider.dart';
 import 'package:twitter_clone/providers/user_provider.dart';
 
 class Home extends ConsumerWidget {
@@ -13,6 +15,10 @@ class Home extends ConsumerWidget {
     LocalUser currentUser = ref.watch(userProvider);
     return Scaffold(
       appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(4),
+          child: Container(color: Colors.grey, height: 1),
+        ),
         leading: Builder(
           builder: (context) {
             return GestureDetector(
@@ -28,11 +34,36 @@ class Home extends ConsumerWidget {
             );
           },
         ),
-        title: Text("Home"),
+        title: Image(image: AssetImage('assets/twitterlogo.png'), width: 50),
       ),
-      body: Column(
-        children: [Text(currentUser.user.email), Text(currentUser.user.name)],
-      ),
+      body: ref
+          .watch(feedProvider)
+          .when(
+            data: (List<Tweet> tweets) {
+              return ListView.separated(
+                separatorBuilder:
+                    (context, index) => Divider(color: Colors.black),
+                itemCount: tweets.length,
+                itemBuilder: (context, count) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      foregroundImage: NetworkImage(tweets[count].profilePic),
+                    ),
+                    title: Text(
+                      tweets[count].name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      tweets[count].tweet,
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  );
+                },
+              );
+            },
+            error: (error, stackTrace) => Center(child: Text('Error')),
+            loading: () => CircularProgressIndicator(),
+          ),
       drawer: Drawer(
         child: Column(
           children: [
