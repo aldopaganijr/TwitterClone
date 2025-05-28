6b08e98443c7cc4f3a62a1284cee1696fad41c48
Aldo Pagani <aldojpagani@gmail.com>
2025-05-27 19:25:55 -0600

    added a animation for the post a tweet button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,32 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rive/rive.dart' hide Image;
 import 'package:twitter_clone/models/tweet.dart';
 import 'package:twitter_clone/pages/create.dart';
 import 'package:twitter_clone/pages/settings.dart';
 import 'package:twitter_clone/providers/tweet_provider.dart';
 import 'package:twitter_clone/providers/user_provider.dart';
 
-class Home extends ConsumerWidget {
+class Home extends ConsumerStatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Home> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends ConsumerState<Home> {
+  bool clicked = false;
+  SMIInput<bool>? _pressed;
+
+  void _onRiveInit(Artboard artboard) {
+    final controller = StateMachineController.fromArtboard(artboard, 'Button');
+    artboard.addController(controller!);
+    _pressed = controller.findInput('Press');
+  }
+
+  @override
+  Widget build(BuildContext context) {
     LocalUser currentUser = ref.watch(userProvider);
     return Scaffold(
       appBar: AppBar(
@@ -34,6 +49,19 @@ class Home extends ConsumerWidget {
             );
           },
         ),
+        actions: [
+          AnimatedOpacity(
+            opacity: clicked ? 1 : 0,
+            duration: Duration(seconds: 1),
+            child: Padding(
+              padding: const EdgeInsets.only(right: 5),
+              child: CircleAvatar(
+                radius: 60,
+                child: Image.asset('assets/mars.png'),
+              ),
+            ),
+          ),
+        ],
         title: Image(image: AssetImage('assets/twitterlogo.png'), width: 50),
       ),
       body: ref
@@ -95,13 +123,38 @@ class Home extends ConsumerWidget {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(
-            context,
-          ).push(MaterialPageRoute(builder: (context) => CreateTweet()));
-        },
-        child: Icon(Icons.add),
+      floatingActionButton: AnimatedContainer(
+        padding: const EdgeInsets.only(top: 90),
+        alignment: clicked ? Alignment.topRight : Alignment.bottomRight,
+        duration: const Duration(seconds: 1),
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: GestureDetector(
+            onTapDown: (_) {
+              setState(() {
+                clicked = true;
+              });
+              _pressed?.value = true;
+              Future.delayed(const Duration(seconds: 2), () {
+                setState(() {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => CreateTweet()),
+                  );
+                  setState(() {
+                    clicked = false;
+                  });
+                });
+              });
+            },
+            onTapCancel: () => _pressed?.value = false,
+            onTapUp: (_) => _pressed?.value = false,
+            child: RiveAnimation.asset(
+              'assets/rocket.riv',
+              onInit: _onRiveInit,
+            ),
+          ),
+        ),
       ),
     );
   }
